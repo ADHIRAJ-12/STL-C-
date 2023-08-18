66151d8b4e3e3fd460034d741976d8e40bc26726
Adhiraj <104266861+ADHIRAJ-12@users.noreply.github.com>
2023-08-18 22:27:27 +0530

    Update Bfs.cpp

--- Bfs.cpp
+++ Bfs.cpp
@@ -1,37 +1,95 @@
-queue<int> q;
 
-vector<bool> used(n);
+vector < vector < int > > nearest(vector < vector < int >> & mat, int n, int m) {
+    // Write your code here.
 
-vector<int> d(n), p(n);
+    vector < vector < int > >vis(n,vector<int>(m,0));
+    vector < vector < int > >dist(n,vector<int>(m,0));
 
-q.push(s);
+    queue<pair<int,int>>q;
 
-used[s] = true;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            if(mat[i][j]==1){
+                q.push({i,j});
+                dist[i][j]=0;
+            }
+        }
+    }
+    vector<int>dx={-1,0,1,0};
+    vector<int>dy={0,-1,0,1};
 
-p[s] = -1;
 
-while (!q.empty()) {
+    while(!q.empty()){
 
-    int v = q.front();
+        int row=q.front().first;
+        int col=q.front().second;
+        q.pop();
+        vis[row][col]=1;
+        
+        for(int i=0;i<4;i++){
+            int nrow=row+dx[i];
+            int ncol=col+dy[i];
 
-    q.pop();
-
-    for (int u : adj[v]) {
-
-        if (!used[u]) {
-
-            used[u] = true;
-
-            q.push(u);
-
-            d[u] = d[v] + 1;
-
-            p[u] = v;
+            if(nrow>=0 && ncol>=0 && nrow<n && ncol<m && !vis[nrow][ncol]&& mat[nrow][ncol]==0){
+                vis[nrow][ncol]=1;
+                mat[nrow][ncol]=1;
+                dist[nrow][ncol]=dist[row][col]+1;
+                q.push({nrow,ncol});
+            }
 
         }
 
+
     }
+        return dist;
+
 
 }
 
-```
+
+
+
+
+
+
+
+
+
+
+// queue<int> q;
+
+// vector<bool> used(n);
+
+// vector<int> d(n), p(n);
+
+// q.push(s);
+
+// used[s] = true;
+
+// p[s] = -1;
+
+// while (!q.empty()) {
+
+//     int v = q.front();
+
+//     q.pop();
+
+//     for (int u : adj[v]) {
+
+//         if (!used[u]) {
+
+//             used[u] = true;
+
+//             q.push(u);
+
+//             d[u] = d[v] + 1;
+
+//             p[u] = v;
+
+//         }
+
+//     }
+
+// }
+
+// ```

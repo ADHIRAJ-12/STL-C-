e57c6b10eced478d64cb6d8a022c15349fd9a4f9
Adhiraj <104266861+ADHIRAJ-12@users.noreply.github.com>
2023-08-26 19:54:06 +0530

    Update DSU.cpp

--- DSU.cpp
+++ DSU.cpp
@@ -1,5 +1,96 @@
-// hacker earth codemonk
+//dsa
+#include <bits/stdc++.h>
+using namespace std;
 
+class DisjointSet{
+
+    vector<int>parent,rank,size;
+
+public:
+    //constructor
+     DisjointSet(int n){
+
+        parent.resize(n+1);
+        rank.resize(n+1,0);
+        size.resize(n+1);
+
+        for(int i=0;i<=n;i++){
+            parent[i]=i;
+            size[i]=1;
+        }
+
+
+     }
+
+    int findUltParent(int node){
+        if(node==parent[node])return node;
+        else return parent[node]=findUltParent(parent[node]);
+    }
+
+
+    void unionByRank(int u,int v){
+
+        int ultU=findUltParent(u);
+        int ultV=findUltParent(v);
+
+        if(ultV==ultU)return;
+        if(rank[ultV]<rank[ultU])parent[ultV]=ultU;
+        else if(rank[ultU]<rank[ultV])parent[ultU]=ultV;
+        else {
+            parent[ultV]=ultU;
+            rank[ultU]++;
+        }
+    }
+
+
+    void unionBySize(int u,int v){
+
+        int ultU=findUltParent(u);
+        int ultV=findUltParent(v);
+
+        if(ultV==ultU)return;
+        if(size[ultV]<size[ultU]){
+            parent[ultV]=ultU;
+            size[ultU]+=size[ultV];
+        }
+        else{
+             parent[ultU]=ultV;
+            size[ultV]+=size[ultU];
+        }
+    }
+
+
+
+
+
+
+} ;
+int main() {
+    DisjointSet ds(7);
+    ds.unionByRank(1, 2); 
+    ds.unionByRank(2, 3); 
+    ds.unionByRank(4, 5); 
+    ds.unionByRank(6, 7); 
+    ds.unionByRank(5, 6); 
+    // if 3 and 7 same or not 
+    if(ds.findUltParent(3) == ds.findUltParent(7)) {
+        cout << "Same\n"; 
+    }
+    else cout << "Not same\n"; 
+
+    ds.unionByRank(3, 7); 
+
+    if(ds.findUltParent(3) == ds.findUltParent(7)) {
+        cout << "Same\n"; 
+    }
+    else cout << "Not same\n"; 
+	return 0;
+};
+
+
+
+
+//cp
  
 #include <bits/stdc++.h>
 using namespace std;
